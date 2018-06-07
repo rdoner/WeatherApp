bce9a555fb36ac997a805059cba44500aeab3d46
Ryan Doner <ridemralphio@yahoo.com>
2018-06-07 14:52:26 -0600

    Initial Deploy

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'df7bbcc50ca29913e364e4584adcbfa7';
+//const API_KEY = 'c0be993379f815c0843ebc2443387a5a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
