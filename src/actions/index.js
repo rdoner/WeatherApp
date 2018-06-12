1921b8561259646dced82254fde1f315e308bb6d
Ryan Doner <ridemralphio@yahoo.com>
2018-06-12 16:36:20 -0600

    second commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'df7bbcc50ca29913e364e4584adcbfa7';
 //const API_KEY = 'c0be993379f815c0843ebc2443387a5a';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
